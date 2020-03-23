0227c7809a02582a5229876eda52394f25a71276
Your Name <you@example.com>
2020-03-23 12:44:15 +0530

    Setup complete

---
+++ sever.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net"
+
+    ""
+    "google.golang.org/grpc"
+)
+
+func main() {
+    lis, err: = net.Listen("tcp", fmt.Sprintf(":%d", 14586))
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+
+    s: = todo.Server {}
+    grpcServer: = grpc.NewServer()
+    // attach the todo service to the server
+    todo.RegisterTodoServiceServer(grpcServer, & s)
+
+    if err: = grpcServer.Serve(lis);
+    err != nil {
+        log.Fatalf("failed to serve: %s", err)
+    } else {
+        log.Printf("Server started successfully")
+    }
+}
